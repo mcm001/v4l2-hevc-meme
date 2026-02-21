25488722e81eb4602c62647ce63398ea25371b85
Matt M <matthew.morley.ca@gmail.com>
2026-02-21 09:40:28 -0800

    Comment

--- rtsp_server.cpp
+++ rtsp_server.cpp
@@ -5,6 +5,8 @@
 // Adapted from
 // https://github.com/wpilibsuite/allwpilib/blob/9cd933fa1494a4e486102b17040d9cf9201b75cd/wpinet/examples/webserver/webserver.cpp#L68
 
+// Test me with "rtsp://127.0.0.1:5801/lifecam" in VLC or similar.
+
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.

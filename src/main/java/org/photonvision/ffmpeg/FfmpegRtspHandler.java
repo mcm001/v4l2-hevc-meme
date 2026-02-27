1a454011133fff24333474a6fe31eb8d0545c4f4
Matt Morley <matthew.morley.ca@gmail.com>
2026-02-26 23:06:03 -0800

    forgot to actually require java compile firstt

--- src/main/java/org/photonvision/ffmpeg/FfmpegRtspHandler.java
+++ src/main/java/org/photonvision/ffmpeg/FfmpegRtspHandler.java
@@ -1,5 +1,11 @@
+// Copyright (c) PhotonVision contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the GNU General Public License Version 3 in the root directory of this project.
+
 package org.photonvision.ffmpeg;
 
 public class FfmpegRtspHandler {
-  public static native int putFrame(String streamName, long matPtr);
+  public static native boolean initialize();
+
+  public static native boolean putFrame(String streamName, long matPtr);
 }
--- src/test/java/org/photonvision/ffmpeg/FfmpegJniTest.java
+++ src/test/java/org/photonvision/ffmpeg/FfmpegJniTest.java
@@ -1,3 +1,7 @@
+// Copyright (c) PhotonVision contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the GNU General Public License Version 3 in the root directory of this project.
+
 package org.photonvision.ffmpeg;
 
 import org.junit.jupiter.api.Test;

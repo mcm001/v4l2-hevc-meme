a47e4ade2905ab888bb0954e9044839cc3438223
Matt Morley <matthew.morley.ca@gmail.com>
2026-03-02 15:22:38 -0600

    huh

--- src/test/java/org/photonvision/ffmpeg/FfmpegJniTest.java
+++ src/test/java/org/photonvision/ffmpeg/FfmpegJniTest.java
@@ -5,6 +5,7 @@
 package org.photonvision.ffmpeg;
 
 import edu.wpi.first.util.CombinedRuntimeLoader;
+import java.nio.file.Path;
 import org.junit.jupiter.api.Test;
 import org.opencv.core.Core;
 import org.opencv.imgcodecs.Imgcodecs;
@@ -12,17 +13,27 @@ import org.opencv.imgcodecs.Imgcodecs;
 class FfmpegJniTest {
     @Test
     public void testMeme() throws Exception {
+        var libPath =
+                "/home/matt/Documents/GitHub/v4l2-hevc-meme/build/libs/rtspServer/shared/linuxx86-64/release/libRtspServer.so";
+        assert Path.of(libPath).toFile().exists();
+
         CombinedRuntimeLoader.loadLibraries(
                 FfmpegJniTest.class, Core.NATIVE_LIBRARY_NAME, "wpiutil", "wpinet");
-        System.load(
-                "/home/matt/Documents/GitHub/v4l2-hevc-meme/build/libs/rtspServer/shared/linuxx86-64/release/libRtspServer.so");
+        try {
+            System.load(
+                    "/home/matt/Documents/GitHub/v4l2-hevc-meme/build/libs/rtspServer/shared/linuxx86-64/release/libRtspServer.so");
+        } catch (UnsatisfiedLinkError e) {
+            e.printStackTrace();
+            throw e;
+        }
 
         var mat =
                 Imgcodecs.imread("/home/matt/Downloads/left-cam_input_2025-07-02T065100975_None-0-.jpg");
 
         FfmpegRtspHandler.initialize();
 
-        while (true) {
+        for (int i = 0; i < 10; i++) {
+            // while (true) {
             FfmpegRtspHandler.putFrame("test", mat.getNativeObjAddr());
             Thread.sleep(1000 / 30);
         }
